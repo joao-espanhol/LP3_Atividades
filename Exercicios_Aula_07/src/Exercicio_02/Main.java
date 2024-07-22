cc642f0c736823399c81a4992ff5ad66e8fa6c4c
João Pedro Espanhol <97205117+joao-espanhol@users.noreply.github.com>
2024-07-22 20:34:32 -0300

    Add files via upload

---
+++ Exercicios_Aula_07/src/Exercicio_02/Main.java
@@ -0,0 +1,29 @@
+package Exercicio_02;
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Insira sua String: ");
+        String frase = scanner.nextLine();
+        
+        frase = frase.toUpperCase();
+        frase = frase.replace(" ", "");
+        
+        int length = frase.length();
+        char[] vogais = {'A', 'E', 'I', 'O', 'U'};
+        int qtdVogais = 0;
+        
+        for (int i = 0; i < length; i++) {
+        	for (int j = 0; j < 5; j ++) {
+        		if (frase.charAt(i) == vogais[j]) {
+        			qtdVogais++;
+        		}
+        	}
+        }
+        System.out.print("Quantidade de vogais: " + qtdVogais);
+
+	}
+}
---
+++ Exercicios_Aula_07/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Exercicios_Aula_07 {
+}
