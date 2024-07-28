58e17aa412356ede2496dc70739dd0ffa59d38be
João Pedro Espanhol <97205117+joao-espanhol@users.noreply.github.com>
2024-07-28 15:31:22 -0300

    Add files via upload

---
+++ Exercicios_Aula_07/src/Exercicio_08/Main.java
@@ -0,0 +1,10 @@
+package Exercicio_08;
+//Escrever um programa que gera um número aleatório entre 1 e 100.
+
+public class Main {
+	public static void main (String[] args) {
+        System.out.println("Random number: " + (int) (Math.random()*100));
+
+
+	}
+}
